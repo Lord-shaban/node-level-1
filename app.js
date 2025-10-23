9ce25573b5d9bd157208d89b42d18a1fb1535eeb
Ahmed shaban <a.shaban2366@nu.edu.eg>
2025-10-23 17:55:08 +0300

    feat: integrate mongoose for MongoDB connection and add new routes
    - Added mongoose dependency for MongoDB integration.
    - Established a connection to MongoDB in app.js.
    - Changed the root route from '/' to '/home' and added a new '/test' route.
    - Updated home.html to reflect a new personal information form with appropriate fields and styles.
    - Modified styles for better responsiveness and user experience.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
@@ -8,10 +9,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.sendFile('./views/home.html', { root: __dirname });
+});
+app.get('/test', (req, res) => {
+  res.send('Test Page');
 });
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
+});
+
+mongoose.connect('mongodb+srv://dearahmed65_db_user:SVE0jlafRQSNyhLR@cluster0.whuf9d6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
 });
