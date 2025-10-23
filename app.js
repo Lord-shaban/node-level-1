c6cb67138142c5f566bcf24e10b68d81d5bc68f2
Ahmed shaban <a.shaban2366@nu.edu.eg>
2025-10-23 20:03:27 +0300

    feat: update app.js with new routes and modify response handling; add ejs dependency in package.json

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+
+mongoose.connect('mongodb+srv://dearahmed65_db_user:SVE0jlafRQSNyhLR@cluster0.whuf9d6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
+
 
 // منع التخزين المؤقت (Cache) للتطوير
 app.use((req, res, next) => {
@@ -13,15 +26,27 @@ app.get('/home', (req, res) => {
   res.sendFile('./views/home.html', { root: __dirname });
 });
 app.get('/test', (req, res) => {
-  res.send('Test Page');
+    let num="";
+    for(let i=0 ; i <= 100 ; i++){
+        num += i + " "
+    }
+  res.send(`${num}`);
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+app.post("/add",(req,res)=>{
+//   res.send(`${req.body.name} hello yor are ${req.query.age} years old`);
+res.json({
+    name: req.body.name,
+    age: req.query.age
+})
 });
 
-mongoose.connect('mongodb+srv://dearahmed65_db_user:SVE0jlafRQSNyhLR@cluster0.whuf9d6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.log(err);
+app.get('/sum/:num1/:num2', (req,res) => {
+const num1 = req.params.num1;
+const num2 = req.params.num2;
+const sum = Number(num1) + Number(num2);
+    res.send(`the sum is ${sum} `);
 });
+
+
+
